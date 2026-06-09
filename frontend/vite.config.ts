f8b2bd1d233a252397b5a8820d439c38c71096c9
Viktor Majewski <viktor19980785@gmail.com>
2026-06-09 06:22:33 +0200

    WH-36 configure vite and add cryptorgraphy dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // It is used for development, and it is not used in production.
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
 })
